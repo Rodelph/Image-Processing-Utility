c01ea92c68c8ddc42f5d04cc526dfb00a5a5ff90
Amine NAQI <anaqi@um6ss.ma>
2021-09-08 13:49:59 +0100

    more stuff

--- build-ImageProc-Desktop-Release/CTestTestfile.cmake
+++ build-ImageProc-Desktop-Release/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(Tester "ImageProc")
-set_tests_properties(Tester PROPERTIES  _BACKTRACE_TRIPLES "/home/rodelph/dev/ImageProc/CMakeLists.txt;44;add_test;/home/rodelph/dev/ImageProc/CMakeLists.txt;0;")
+set_tests_properties(Tester PROPERTIES  _BACKTRACE_TRIPLES "/home/rodelph/dev/ImageProc/CMakeLists.txt;55;add_test;/home/rodelph/dev/ImageProc/CMakeLists.txt;0;")
