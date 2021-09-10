d6b8b16dcbe54cd7cf22269c5aa870a1349d6ada
Amine NAQI <anaqi@um6ss.ma>
2021-09-10 23:16:58 +0100

    added opencv makelist

--- build-ImageProc-Desktop-Release/ProjectEx/CMakeFiles/ImageProc.dir/DependInfo.cmake
+++ build-ImageProc-Desktop-Release/ProjectEx/CMakeFiles/ImageProc.dir/DependInfo.cmake
@@ -29,6 +29,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../FiltOption/LowPassFilt/src"
   "../HistOption/src"
   "../ImageAlgorithms/src"
+  "/usr/local/include/opencv4"
   "/usr/include/x86_64-linux-gnu/qt5"
   "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
   "/usr/include/x86_64-linux-gnu/qt5/QtGui"
